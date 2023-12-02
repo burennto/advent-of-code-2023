b04a212c92482930ef3c72dbd73927426fedd4e7
Brent Chuang <421829+burennto@users.noreply.github.com>
2023-12-02 22:17:05 +1000

    Handle sample input for part 2

--- lib/solution_base.rb
+++ lib/solution_base.rb
@@ -1,6 +1,10 @@
 class SolutionBase
-  def sample_input
-    File.read(File.join(__dir__, '..', 'src', module_dir, 'sample.txt'))
+  def sample_input_one
+    File.read(File.join(__dir__, '..', 'src', module_dir, 'sample1.txt'))
+  end
+
+  def sample_input_two
+    File.read(File.join(__dir__, '..', 'src', module_dir, 'sample2.txt'))
   end
 
   def actual_input
